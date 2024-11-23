4c5c697e8dedc329580bb4ea9d935d722a43d0a9
alireza <alirezat775@gmail.com>
2024-11-24 01:20:16 +0300

    update HelloController in api-php

--- api-php/src/Controllers/HelloController.php
+++ api-php/src/Controllers/HelloController.php
@@ -4,16 +4,9 @@ namespace App\Controllers;
 
 class HelloController
 {
-    private function sendJson($data = [], $status = 200): void
-    {
-        header("Content-Type: application/json");
-        http_response_code($status);
-        echo json_encode($data);
-    }
-
     public function index(): void
     {
-        $this->sendJson(['message' => 'Hello, World!']);
+        echo("Hello world");
     }
 
 }
